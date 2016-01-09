43cb9e25d3edebc27d91e510fb1dc7c278f69045
Daniel Porter <danielbowenporter@gmail.com>
2016-01-09 01:10:19 -0600

    Progress. Finally got AppBar to stretch across the whole page.

--- client-side/web-app/src/index.js
+++ client-side/web-app/src/index.js
@@ -3,11 +3,16 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import App from './components/App';
+
 const store = configureStore();
 
 render(
   <Provider store={store}>
-    <h1>Hello World.</h1>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
