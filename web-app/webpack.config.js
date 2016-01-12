6d4d614129b7cd90df61f76bdeff46212d1a2edd
Jensen Dempsey <jdemp@jensendempsey.com>
2016-01-12 15:49:12 -0600

    moved the web-app out of static and made the build path into static

--- web-app/webpack.config.js
+++ web-app/webpack.config.js
@@ -2,20 +2,20 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: './build',
-    filename: 'bundle.js',
+    path: '../static/build',
+    filename: 'bundle.js'
   },
 
   module: {
     loaders: [
       { test: [/\.(js|jsx)$/], exclude: /node_modules/, loader: 'babel-loader' },
-      { test: /\.json/, loader: 'json-loader' },
-    ],
+      { test: /\.json/, loader: 'json-loader' }
+    ]
   },
 
   devtool: '#inline-source-map',
 
   resolve: {
-    extensions: ['', '.js', '.jsx'],
-  },
+    extensions: ['', '.js', '.jsx']
+  }
 };
