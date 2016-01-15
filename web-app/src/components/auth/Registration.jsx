bda0ab815d21ca2e216fefb32955d4224ccb01aa
Jensen Dempsey <jdemp@jensendempsey.com>
2016-01-15 17:17:40 -0600

    adding a basic registration component and a rpg-arc css file for any general css we need
    it does nothing but does exist,

--- web-app/src/components/auth/Registration.jsx
+++ web-app/src/components/auth/Registration.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 class Registration extends React.Component {
 
   render() {
     return (
-      <div>
-          <h1>This is the registration page</h1>
+      <div className="mdl-card mdl-shadow--4dp center">
+          <div className="mdl-card__title">
+               <h3>Registration</h3>
+            </div>
+        <div className="mdl-card__supporting-text">
+          <TextField
+            hintText="First Name"
+            floatingLabelText="First Name"
+            type="text" />
+          <br/>
+          <TextField
+            hintText="Last Name"
+            floatingLabelText="Last Name"
+            type="text" />
+          <br/>
+          <TextField
+            hintText="Email"
+            floatingLabelText="Email"
+            type="text" />
+          <br/>
+        <TextField
+            hintText="Password Field"
+            floatingLabelText="Password"
+            type="password" />
+          <br/>
+            <br/>
+         <RaisedButton label="Primary" primary={true} />
         </div>
+      </div>
     );
   }
 }
