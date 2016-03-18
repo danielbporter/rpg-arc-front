0ee5848f7447dec12f46c2f8a9ce69e7c188d9d2
Daniel Porter <danielbowenporter@gmail.com>
2016-03-18 10:26:52 -0500

    webpack changes.

--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,22 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
-    path: '../static/build',
-    filename: 'bundle.js'
+    path: 'build/',
+    filename: 'bundle.js',
   },
 
   module: {
     loaders: [
       { test: [/\.(js|jsx)$/], exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.json/, loader: 'json-loader' },
-       { test: /\.css$/, loader: "style-loader!css-loader" },
-      {test: /\.scss$/, loaders: ["style", "css", "sass"]}
-    ]
+      { test: /\.css$/, loaders: ['style', 'css', 'loader'] },
+      { test: /\.scss$/, loaders: ['style', 'css', 'sass'] },
+    ],
   },
 
   devtool: '#inline-source-map',
 
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css']
-  }
+    extensions: ['', '.js', '.jsx', '.css'],
+  },
 };
